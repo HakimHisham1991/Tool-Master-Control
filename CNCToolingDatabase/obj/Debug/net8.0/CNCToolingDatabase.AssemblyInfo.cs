f8b8e549fedc586919baf6af897c633e6e9ada33
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-01-28 16:22:16 +0800

    Enhance Tool List functionality by adding 'No. of Tooling' field
    - Introduced a new column 'No. of Tooling' in the Tool List export and Index view for better data representation.
    - Updated ToolListService to calculate and include tooling counts for each tool list item.
    - Modified ToolListRepository to fetch tooling counts based on header IDs, improving data accuracy.
    - Adjusted related view models and repository interfaces to support the new tooling data.

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81504b68dd045b7e0407be50a39bbe3f1474b933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a378a32462c4cd11d70723d5120b52756d7f690d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
