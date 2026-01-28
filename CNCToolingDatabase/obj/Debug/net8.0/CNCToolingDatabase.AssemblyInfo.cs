9c8200623b415159b7efd9781f239116e6d7d273
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-01-28 12:12:37 +0800

    Update settings data and fix remarks bug

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e54580f626d24c91c117f5258eb2c9f53046d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a2fc63716fcc0ab83186d50497fd59c251ecba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
