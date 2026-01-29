5bb6c85995e2b73f5678bd851d4faaa9f0fe7630
HakimHisham1991 <hakim.hisham.1991@gmail.com>
2026-01-29 18:22:25 +0800

    Refactor Tool List Editor for enhanced Part Number handling and user experience
    - Improved the GetPartNumbers method to ensure distinct part numbers are aggregated from both the PartNumbers table and tool list headers.
    - Added error handling for scenarios where the PartNumbers table may be missing, enhancing application robustness.
    - Introduced a new JavaScript variable for the current part number in the Tool List Editor view to improve user interaction.

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0553d34b55554bd501abe872de009a0c6a5978ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1013abe3fdf02a72e1683f666ce41c611b70502")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
