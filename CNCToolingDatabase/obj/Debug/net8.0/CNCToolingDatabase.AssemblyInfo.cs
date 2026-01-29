e3f247bfb5135fed263d7da75a36e56862cdf149
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-01-29 16:11:55 +0800

    Refactor ToolCodeUnique views for improved consistency and user experience
    - Updated terminology in ToolCode and ToolCodeUnique views to ensure clarity and consistency across the application.
    - Enhanced navigation by refining action buttons and layout, making it easier for users to interact with the ToolCodeUnique features.
    - Improved documentation comments to align with the recent changes, maintaining a coherent codebase.

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996b36a516978052ae49448ac32aa1ee54054cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb34af6c10597aca333bbdd0abc9b837eda45ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
