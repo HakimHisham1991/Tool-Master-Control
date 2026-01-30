969e073619b2807272976621e8804bd62da4aff2
HakimHisham1991 <hakim.hisham.1991@gmail.com>
2026-01-31 02:08:39 +0800

    Refactor ToolListEditor to enhance machine name management and improve user experience
    - Updated the Index.cshtml view to clarify the behavior of the Machine Name dropdown, ensuring it is set by Machine Name Management.
    - Enhanced JavaScript functionality to improve the interaction with the Machine Name selection, providing a more intuitive user experience.
    - Modified the ToolListEditorController to ensure seamless integration of machine name data in the selection process.

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f943b3409ddbfd77eba6e40c007d323a7af270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d580617d89554e6681f26bf6ceb1cb8e666a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
