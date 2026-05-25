13bb804db1e8e4110dd0523f40001e85b3f37eed
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-05-25 13:04:26 +0800

    Initial commit - force replace with local state

--- CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
+++ CNCToolingDatabase/obj/Debug/net8.0/CNCToolingDatabase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d580617d89554e6681f26bf6ceb1cb8e666a88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7239cff87cb54102466003a861b24232b5838a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCToolingDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
